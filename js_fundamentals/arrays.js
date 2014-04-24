59700b266a85de5bfe7046f7d0d7bb8277f794c9
allaboutdrama <allaboutdramaproductions@gmail.com>
2014-04-23 23:44:23 -0400

    Completed Array Exercise

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -7,4 +7,16 @@
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
 
 
+document.write("<h2>" + "My Top Choices" + "</h2>")
 
+var faveThings = ["blue", "2001 Space Odyssey", "La Habana"];
+var faveCategories = ["colour", "movie", "city"];
+
+for (i = 0; i < faveThings.length;){
+  for(j = 0; i < faveCategories.length; j++)
+{
+  document.write("My favorite " + faveCategories[i] + " is " + faveThings[i]);
+  document.write("<br>");
+  i++;
+}
+}
